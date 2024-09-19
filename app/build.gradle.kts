c729f5b4b4684cccfd53a80f563300cb0ddce6f6
lighttigerxiv <lighttigerxiv@gmail.com>
2024-09-19 16:10:33 +0100

    change version code XD

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.whiskersapps.clawlauncher"
         minSdk = 29
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
